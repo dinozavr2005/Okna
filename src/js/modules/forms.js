789f2c11557040672b52dd5e8e3896fa82a47eb2
Vladimir Buikliskii <papa31@yandex.ru>
2021-08-02 13:41:35 +0300

    done

---
+++ src/js/modules/forms.js
@@ -0,0 +1,64 @@
+import checkNumInputs from "./checkNumInputs";
+
+const forms = (state) => {
+    const form = document.querySelectorAll('form'),
+    inputs = document.querySelectorAll('input');
+
+    checkNumInputs('input[name="user_phone"]');
+    
+
+    const message = {
+        loading: 'Загрузка...',
+        success: 'Отправлено!',
+        failure: 'Упс! Что то пошло не так'
+    };
+
+    const postData = async (url, data) => {
+        document.querySelector('.status').textContent = message.loading;
+        let res = await fetch(url, {
+            method: "POST",
+            body: data
+        });
+
+        return await res.text();
+    };
+
+    const clearInputs = () => {
+        clearInputs.forEach(item => {
+            item.value = '';
+        })
+    };
+
+    form.forEach(item => {
+        item.addEventListener('submit', (e) => {
+            e.preventDefault();
+
+            let statusMessage = document.createElement('div');
+            statusMessage.classList.add('status');
+            item.appendChild(statusMessage);
+
+            const formData = new FormData(item);
+            if (item.getAttribute('data-calc') === "end") {
+                for (let key in state) {
+                    formData.append(key, state[key]);
+                }
+            }
+
+            postData('assets/server.php', formData)
+            .then(res => {
+                console.log(res);
+                statusMessage.textContent = message.success;
+            })
+            .catch(() => statusMessage.textContent = message.failure)
+            .finally(() => {
+                item.reset()
+                setTimeout(() => {
+                    statusMessage.remove();
+                }, 5000);
+            })
+
+        });
+    });
+};
+
+export default forms;
